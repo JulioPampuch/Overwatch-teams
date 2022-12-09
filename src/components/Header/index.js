5aaef956167e91e896ed39ea0d21e49dbba0a19d
Julio Pampuch <juliopampuch@gmail.com>
2022-12-09 11:34:37 -0300

    updated images link

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header>
       <div>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Overwatch_circle_logo.svg/2048px-Overwatch_circle_logo.svg.png" />
+        <img src="/imagens/logo.png" />
         <h1>OVERWATCH</h1>
         <h2>TEAMS</h2>
       </div>
