cbb3d972d473a919d4edf7e1bd5ea9a28e0dae81
Julio Pampuch <juliopampuch@gmail.com>
2022-12-09 08:50:59 -0300

    created header

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import "./header.css"
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Overwatch_circle_logo.svg/2048px-Overwatch_circle_logo.svg.png" />
+        <h1>Overwatch</h1>
+        <h2>teams</h2>
+      </div>
+    </header>
+  )
+}
+
+export default Header
