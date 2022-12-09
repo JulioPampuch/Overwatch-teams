73fcfcb5edddfd0c339f98bab62590177e87ef50
Julio Pampuch <juliopampuch@gmail.com>
2022-12-09 11:21:50 -0300

    updated fonts

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,8 +5,8 @@ const Header = () => {
     <header>
       <div>
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Overwatch_circle_logo.svg/2048px-Overwatch_circle_logo.svg.png" />
-        <h1>Overwatch</h1>
-        <h2>teams</h2>
+        <h1>OVERWATCH</h1>
+        <h2>TEAMS</h2>
       </div>
     </header>
   )
