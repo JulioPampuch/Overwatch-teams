040ffb8cdeae75d46e437f164fbed70a5b94e0f3
Julio Pampuch <juliopampuch@gmail.com>
2022-12-10 10:54:13 -0300

    created form inputs

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -5,6 +5,22 @@ const Form = () => {
     <section className="sectionForm">
       <h3>Preencha os dados do seu herói.</h3>
       <form>
+        <div>
+          <label for="nome">Nome</label>
+          <input type="text" name="nome" />
+        </div>
+        <div>
+          <label for="imagem">Imagem</label>
+          <input type="text" name="imagem" />
+        </div>
+        <div>
+          <label for="funcao">Função</label>
+          <select name="funcao" />
+        </div>
+        <div>
+          <label for="time">Time</label>
+          <select name="time" />
+        </div>
       </form>
     </section>
   )
