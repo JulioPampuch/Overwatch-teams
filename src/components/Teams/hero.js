1df541501c819ef2af60b78af713d0a6ef8c67de
Julio Pampuch <juliopampuch@gmail.com>
2022-12-14 22:02:53 -0300

    created remove team button

--- src/components/Teams/hero.js
+++ src/components/Teams/hero.js
@@ -1,10 +1,10 @@
 const Hero = (props) => {
+
   return (
     <div className="card">
       <div className="card-header" style={{ backgroundColor: props.primaryColor }}>
         <div className="card-img">
           <div className="bg-img" style={{ backgroundImage: `url(${props.image})` }}></div>
-          {/* <img src={props.image} alt="Hero" /> */}
         </div>
       </div>
       <div className="card-footer">
--- src/components/Teams/index.js
+++ src/components/Teams/index.js
@@ -3,6 +3,10 @@ import "./teams.css"
 
 const Teams = (props) => {
 
+  const removeTeam = (e) => {
+    e.target.parentNode.remove()
+  }
+
   return (
     props.heroes.length >= 1 && props.heroes.length < 6 ?
       <section className="teams" style={{ backgroundColor: props.secundaryColor }}>
@@ -11,24 +15,25 @@ const Teams = (props) => {
         {props.heroes.map((hero) => {
           return (
             <Hero key={hero.nome} heroName={hero.nome} heroFunction={hero.funcao} image={hero.imagem} primaryColor={props.primaryColor} />
-          )
-        })}
-      </section>
+            )
+          })}
+          <button className="remove-team" onClick={removeTeam} style={{ backgroundColor: props.primaryColor }}>Remover time</button>
+      </section >
       :
-      props.heroes.length >= 6 ?
-        <>
-          <section className="teams" style={{ backgroundColor: props.secundaryColor }}>
-            <h2 className="team-title" style={{ color: props.primaryColor }}>{props.teamTitle}</h2>
-            <p className="team-error">Atenção! Um time tem apenas 5 heróis</p>
-            {props.heroes.map((hero) => {
-              return (
-                <Hero key={hero.nome} heroName={hero.nome} heroFunction={hero.funcao} image={hero.imagem} primaryColor={props.primaryColor} />
-              )
-            })}
-          </section>
-        </> :
-        <>
-        </>
+props.heroes.length >= 6 ?
+  <>
+    <section className="teams" style={{ backgroundColor: props.secundaryColor }}>
+      <h2 className="team-title" style={{ color: props.primaryColor }}>{props.teamTitle}</h2>
+      <p className="team-error">Atenção! Um time tem apenas 5 heróis</p>
+      {props.heroes.map((hero) => {
+        return (
+          <Hero key={hero.nome} heroName={hero.nome} heroFunction={hero.funcao} image={hero.imagem} primaryColor={props.primaryColor} />
+        )
+      })}
+    </section>
+  </> :
+  <>
+  </>
   )
 }
 
