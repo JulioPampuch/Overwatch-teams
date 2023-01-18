a57b230f7155ef9ce253d021840b89197338790d
Julio Pampuch <juliopampuch@gmail.com>
2023-01-17 22:26:33 -0300

    fixed identation errors

--- src/components/Teams/index.js
+++ src/components/Teams/index.js
@@ -15,25 +15,25 @@ const Teams = (props) => {
         {props.heroes.map((hero) => {
           return (
             <Hero key={hero.nome} heroName={hero.nome} heroFunction={hero.funcao} image={hero.imagem} primaryColor={props.primaryColor} />
-            )
-          })}
-          <button className="remove-team" onClick={removeTeam} style={{ backgroundColor: props.primaryColor }}>Remover time</button>
+          )
+        })}
+        <button className="remove-team" onClick={removeTeam} style={{ backgroundColor: props.primaryColor }}>Remover time</button>
       </section >
       :
-props.heroes.length >= 6 ?
-  <>
-    <section className="teams" style={{ backgroundColor: props.secundaryColor }}>
-      <h2 className="team-title" style={{ color: props.primaryColor }}>{props.teamTitle}</h2>
-      <p className="team-error">Atenção! Um time tem apenas 5 heróis</p>
-      {props.heroes.map((hero) => {
-        return (
-          <Hero key={hero.nome} heroName={hero.nome} heroFunction={hero.funcao} image={hero.imagem} primaryColor={props.primaryColor} />
-        )
-      })}
-    </section>
-  </> :
-  <>
-  </>
+      props.heroes.length >= 6 ?
+        <>
+          <section className="teams" style={{ backgroundColor: props.secundaryColor }}>
+            <h2 className="team-title" style={{ color: props.primaryColor }}>{props.teamTitle}</h2>
+            <p className="team-error">Atenção! Um time tem apenas 5 heróis</p>
+            {props.heroes.map((hero) => {
+              return (
+                <Hero key={hero.nome} heroName={hero.nome} heroFunction={hero.funcao} image={hero.imagem} primaryColor={props.primaryColor} />
+              )
+            })}
+          </section>
+        </> :
+        <>
+        </>
   )
 }
 
